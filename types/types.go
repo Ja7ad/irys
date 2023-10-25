9b3782a4d04041f927173bf2062d838467bb80a2
Javad <ja7ad@live.com>
2023-10-25 09:19:55 +0330

    fix: update response structure

---
+++ types/types.go
@@ -0,0 +1,62 @@
+package types
+
+import (
+	"io"
+	"math/big"
+	"net/http"
+)
+
+type NodeInfo struct {
+	Version   string            `json:"version"`
+	Addresses map[string]string `json:"addresses"`
+	Gateway   string            `json:"gateway"`
+}
+
+type BalanceResponse struct {
+	Balance string `json:"balance"`
+}
+
+type TxToBalanceRequest struct {
+	TxId string `json:"tx_id"`
+}
+
+type TopUpConfirmationResponse struct {
+	Confirmed bool `json:"confirmed"`
+}
+
+type TopUpConfirmation struct {
+	Confirmed bool     `json:"confirmed"`
+	Hash      string   `json:"hash"`
+	Balance   *big.Int `json:"balance"`
+}
+
+type Transaction struct {
+	ID        string `json:"id"`
+	Currency  string `json:"currency"`
+	Address   string `json:"address"`
+	Owner     string `json:"owner"`
+	Signature string `json:"signature"`
+	Target    string `json:"target"`
+	Tags      []Tag  `json:"tags"`
+	Anchor    string `json:"anchor"`
+	DataSize  string `json:"data_size"`
+	RawSize   string `json:"raw_size"`
+}
+
+type File struct {
+	Data          io.ReadCloser
+	Header        http.Header
+	ContentLength int64
+	ContentType   string
+}
+
+func (b BalanceResponse) ToBigInt() *big.Int {
+	bInt := new(big.Int)
+
+	n, ok := bInt.SetString(b.Balance, 10)
+	if !ok {
+		return bInt
+	}
+
+	return n
+}
