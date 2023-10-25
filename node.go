03bed9b461400b36e0e21124abd1c9de7985f5df
Javad <ja7ad@live.com>
2023-10-25 09:19:19 +0330

    add: tag type for arweave tags

---
+++ node.go
@@ -0,0 +1,9 @@
+package irys
+
+type Node string
+
+const (
+	DefaultNode1  Node = "https://node1.irys.xyz"  // DefaultNode1 is node 1 irys
+	DefaultNode2  Node = "https://node2.irys.xyz"  // DefaultNode2 is node 2 irys
+	DefaultDevNet Node = "https://devnet.irys.xyz" // DefaultDevNet is a testnet for test irys
+)
