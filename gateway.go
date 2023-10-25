3a19d0337090665a8ca30de0539926b068616137
Javad <ja7ad@live.com>
2023-10-25 09:18:40 +0330

    feat: add balance, topup, basic upload

--- gateway.go
+++ gateway.go
@@ -2,22 +2,24 @@ package irys
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/Ja7ad/irys/errors"
+	"github.com/Ja7ad/irys/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"io"
 	"math/big"
 	"net/http"
-
-	"github.com/warp-contracts/syncer/src/utils/arweave"
-	"github.com/warp-contracts/syncer/src/utils/bundlr"
 )
 
 const (
-	_pricePath    = "%s/price/%s/%v"
-	_uploadPath   = "%s/tx/%s"
-	_txPath       = "%s/tx/%s"
-	_downloadPath = "%s/%s"
+	_pricePath       = "%s/price/%s/%v"
+	_uploadPath      = "%s/tx/%s"
+	_txPath          = "%s/tx/%s"
+	_downloadPath    = "%s/%s"
+	_sendTxToBalance = "%s/account/balance/matic"
+	_getBalance      = "%s/account/balance/matic?address=%s"
 )
 
 func (i *Irys) GetPrice(fileSize int) (*big.Int, error) {
@@ -38,16 +40,137 @@ func (i *Irys) GetPrice(fileSize int) (*big.Int, error) {
 	return decodeBody[*big.Int](resp.Body)
 }
 
-func (i *Irys) Upload(file io.Reader, tags ...Tag) (Transaction, error) {
+func (i *Irys) GetBalance() (*big.Int, error) {
+	pbKey := i.token.GetPublicKey()
+	url := fmt.Sprintf(_getBalance, i.network, crypto.PubkeyToAddress(*pbKey).Hex())
+
+	resp, err := i.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
+	}
+
+	b, err := decodeBody[types.BalanceResponse](resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.ToBigInt(), nil
+}
+
+func (i *Irys) TopUpBalance(ctx context.Context, amount *big.Int) (types.TopUpConfirmation, error) {
+	urlConfirm := fmt.Sprintf(_sendTxToBalance, i.network)
+
+	hash, err := i.createTx(ctx, amount)
+	if err != nil {
+		return types.TopUpConfirmation{}, err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return types.TopUpConfirmation{}, ctx.Err()
+		default:
+			b, err := json.Marshal(&types.TxToBalanceRequest{
+				TxId: hash,
+			})
+			if err != nil {
+				return types.TopUpConfirmation{}, err
+			}
+			resp, err := i.client.Post(urlConfirm, "application/json", bytes.NewBuffer(b))
+			if err != nil {
+				return types.TopUpConfirmation{}, err
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				b, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return types.TopUpConfirmation{}, err
+				}
+				return types.TopUpConfirmation{}, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
+			}
+
+			confirm, err := decodeBody[types.TopUpConfirmationResponse](resp.Body)
+			if err != nil {
+				return types.TopUpConfirmation{}, err
+			}
+
+			if confirm.Confirmed {
+				balance, err := i.GetBalance()
+				if err != nil {
+					return types.TopUpConfirmation{}, err
+				}
+				return types.TopUpConfirmation{
+					Confirmed: true,
+					Hash:      hash,
+					Balance:   balance,
+				}, nil
+			}
+		}
+	}
+}
+
+func (i *Irys) BasicUpload(ctx context.Context, file io.Reader, tags ...types.Tag) (types.Transaction, error) {
 	url := fmt.Sprintf(_uploadPath, i.network, i.token.GetBundlrName())
 	b, err := io.ReadAll(file)
 	if err != nil {
-		return Transaction{}, err
+		return types.Transaction{}, err
 	}
+
+	price, err := i.GetPrice(len(b))
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	balance, err := i.GetBalance()
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	if balance.Cmp(price) < 0 {
+		_, err := i.TopUpBalance(ctx, price)
+		if err != nil {
+			return types.Transaction{}, err
+		}
+	}
+
 	return i.upload(url, b, tags...)
 }
 
-func (i *Irys) Download(hash string) (*File, error) {
+func (i *Irys) Upload(ctx context.Context, file io.Reader, price *big.Int, tags ...types.Tag) (types.Transaction, error) {
+	url := fmt.Sprintf(_uploadPath, i.network, i.token.GetBundlrName())
+	b, err := io.ReadAll(file)
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	if price == nil {
+		price, err = i.GetPrice(len(b))
+		if err != nil {
+			return types.Transaction{}, err
+		}
+	}
+
+	balance, err := i.GetBalance()
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	if balance.Cmp(price) < 0 {
+		return types.Transaction{}, errors.ErrBalanceIsLow
+	}
+
+	return i.upload(url, b, tags...)
+}
+
+func (i *Irys) Download(hash string) (*types.File, error) {
 	url := fmt.Sprintf(_downloadPath, i.network, hash)
 	resp, err := i.client.Get(url)
 	if err != nil {
@@ -62,7 +185,7 @@ func (i *Irys) Download(hash string) (*File, error) {
 		return nil, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
 	}
 
-	return &File{
+	return &types.File{
 		Data:          resp.Body,
 		Header:        resp.Header,
 		ContentLength: resp.ContentLength,
@@ -70,105 +193,58 @@ func (i *Irys) Download(hash string) (*File, error) {
 	}, nil
 }
 
-func (i *Irys) GetMetaData(hash string) (Transaction, error) {
+func (i *Irys) GetMetaData(hash string) (types.Transaction, error) {
 	url := fmt.Sprintf(_txPath, i.network, hash)
 	resp, err := i.client.Get(url)
 	if err != nil {
-		return Transaction{}, err
+		return types.Transaction{}, err
 	}
 
 	if resp.StatusCode != http.StatusOK {
 		b, err := io.ReadAll(resp.Body)
 		if err != nil {
-			return Transaction{}, err
+			return types.Transaction{}, err
 		}
-		return Transaction{}, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
+		return types.Transaction{}, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
 	}
 
-	return decodeBody[Transaction](resp.Body)
+	return decodeBody[types.Transaction](resp.Body)
 }
 
-func (i *Irys) upload(url string, payload []byte, tags ...Tag) (Transaction, error) {
-	var signer bundlr.Signer
-	bundlrTags := make(bundlr.Tags, 0)
+func (i *Irys) upload(url string, payload []byte, tags ...types.Tag) (types.Transaction, error) {
+	tags = addContentType(http.DetectContentType(payload), tags...)
 
-	for _, tag := range tags {
-		bundlrTags = append(bundlrTags, bundlr.Tag(tag))
+	dataItem := types.BundleItem{
+		Data: types.Base64String(payload),
+		Tags: tags,
 	}
 
-	bundlrTags = addContentType(http.DetectContentType(payload), bundlrTags)
-
-	switch i.token.GetBundlrName() {
-	case "matic", "ethereum":
-		s, err := bundlr.NewEthereumSigner(i.token.GetPrivate())
-		if err != nil {
-			return Transaction{}, err
-		}
-		signer = s
-	case "arweave":
-		s, err := bundlr.NewArweaveSigner(i.token.GetPrivate())
-		if err != nil {
-			return Transaction{}, err
-		}
-		signer = s
-	default:
-		return Transaction{}, errors.ErrBundlrIsInvalid
-	}
-
-	dataItem := bundlr.BundleItem{
-		Data: arweave.Base64String(payload),
-		Tags: bundlrTags,
-	}
-
-	if err := dataItem.Sign(signer); err != nil {
-		return Transaction{}, err
+	if err := dataItem.Sign(i.token.GetSinger()); err != nil {
+		return types.Transaction{}, err
 	}
 
 	reader, err := dataItem.Reader()
 	if err != nil {
-		return Transaction{}, err
+		return types.Transaction{}, err
 	}
 
 	b, err := io.ReadAll(reader)
 	if err != nil {
-		return Transaction{}, err
+		return types.Transaction{}, err
 	}
 
-	body := bytes.NewBuffer(b)
-
-	resp, err := i.client.Post(url, "application/octet-stream", body)
+	resp, err := i.client.Post(url, "application/octet-stream", bytes.NewBuffer(b))
 	if err != nil {
-		return Transaction{}, err
+		return types.Transaction{}, err
 	}
 
 	if resp.StatusCode != http.StatusOK {
 		b, err := io.ReadAll(resp.Body)
 		if err != nil {
-			return Transaction{}, err
+			return types.Transaction{}, err
 		}
-		return Transaction{}, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
+		return types.Transaction{}, fmt.Errorf("%d: %s", resp.StatusCode, string(b))
 	}
 
-	return decodeBody[Transaction](resp.Body)
-}
-
-func decodeBody[T any](body io.Reader) (T, error) {
-	var resp T
-	d := json.NewDecoder(body)
-	return resp, d.Decode(&resp)
-}
-
-func addContentType(contentType string, tags bundlr.Tags) []bundlr.Tag {
-	found := false
-	for _, tag := range tags {
-		if tag.Name == "Content-Type" {
-			found = true
-		}
-	}
-
-	if !found {
-		tags = append(tags, bundlr.Tag{Name: "Content-Type", Value: contentType})
-	}
-
-	return tags
+	return decodeBody[types.Transaction](resp.Body)
 }
