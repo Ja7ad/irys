7047c07871073fc31932846da033f18e05e1608c
Javad <ja7ad@live.com>
2023-10-25 09:20:07 +0330

    add: balance and toptop example

---
+++ _example/balance/balance.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/Ja7ad/irys"
+	"github.com/Ja7ad/irys/token"
+	"log"
+	"math/big"
+)
+
+func main() {
+	matic, err := token.NewMatic(
+		"foo",
+		"https://example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c, err := irys.New(irys.DefaultNode1, matic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := c.GetBalance()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(b.String())
+
+	conf, err := c.TopUpBalance(context.Background(), big.NewInt(2000000000000000))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(conf)
+}
