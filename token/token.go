47b31b6654d9f14deff7ae6c87b193d14e005e6a
Javad <ja7ad@live.com>
2023-10-25 09:19:38 +0330

    fix: seprated Ether in token interface

--- token/token.go
+++ token/token.go
@@ -1,10 +1,50 @@
 package token
 
+import (
+	"crypto/ecdsa"
+	"github.com/Ja7ad/irys/signer"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
 const _0x_prefix = "0x"
 
+type TokenType uint8
+
+const (
+	ETHEREUM TokenType = iota
+	MATIC
+	BNB
+	ARWEAVE
+)
+
 type Token interface {
+	Ether
 	GetBundlrName() string
 	GetChain() string
 	GetSymbol() string
-	GetPrivate() string
+	GetSinger() signer.Signer
+	GetRPCAddr() string
+	GetType() TokenType
+}
+
+type Ether interface {
+	GetRPCClient() *ethclient.Client
+	GetPrivateKey() *ecdsa.PrivateKey
+	GetPublicKey() *ecdsa.PublicKey
+}
+
+type unimplementedEther struct{}
+
+var _ Ether = (*unimplementedEther)(nil)
+
+func (u unimplementedEther) GetRPCClient() *ethclient.Client {
+	panic("implement me")
+}
+
+func (u unimplementedEther) GetPrivateKey() *ecdsa.PrivateKey {
+	panic("implement me")
+}
+
+func (u unimplementedEther) GetPublicKey() *ecdsa.PublicKey {
+	panic("implement me")
 }
